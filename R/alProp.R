e374b764daa11666ff561c25075b8dbc037ad5f7
Jakub Tkaczuk <jtkaczuk@ethz.ch>
2023-06-08 16:12:46 +0200

    Write package documentation

---
+++ R/alProp.R
@@ -0,0 +1,14 @@
+#' Thermodynamic properties of Aluminum
+#'
+#' This dataset contains temperature-dependent thermodynamic properties of Aluminum of 99.99% purity.
+#' It contains temperature, density, cp, thermal conductivity, and thermal expansion coefficient.
+#'
+#' @format A dataframe with 5 variables and 599 observations.
+#' \describe{
+#'   \item{T/K}{temperature}
+#'   \item{rho/(kg/m3)}{density}
+#'   \item{cp/(J/(kg.K))}{isobaric specific heat}
+#'   \item{K/(W/(m.K))}{thermal conductivity}
+#'   \item{alpha_thermal/(1/K)}{thermal expansion coefficient}
+#' }
+"alProp"
